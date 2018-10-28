67cf6108ecd06bcae1985785c7858739e3b3536f
Timo Savola <timo.savola@iki.fi>
2018-10-28 15:14:53 +0200

    wasys: it's not a Package package

--- cmd/wasys/main.go
+++ cmd/wasys/main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package wasys implements a standalone toy compiler and runtime.
+// Program wasys implements a standalone toy compiler and runtime.
 package main
 
 import (
