gate-computer/wag

commit a88c38c4cce53b784832276a5027ad6ef3c60c0a

x86: don't reuse memory trap call sites
It gives more useful stack traces.

internal/isa/x86/memory.go
@@ -124,6 +124,8 @@ func checkAccess(f *gen.Func, sizeReach uint64, index operand.O, offset uint32) 
 		}
 
 		if reachAddr < uint64(f.Module.MemoryLimitValues.Initial) {
+			// Call site is not mapped, so this optimization is part of
+			// portable ABI.
 			base = in.BaseMemory
 			disp = int32(addr)
 			return
@@ -141,22 +143,16 @@ func checkAccess(f *gen.Func, sizeReach uint64, index operand.O, offset uint32) 
 		}
 	}
 
+	var checked link.L
+
 	in.CMP.RegReg(&f.Text, wa.I64, RegScratch, RegMemoryLimit)
+	in.JLcb.Stub8(&f.Text)
+	checked.AddSite(f.Text.Addr)
 
-	if addr := f.MemoryOutOfBounds.Addr(f); addr != 0 {
-		in.JGEcb.Addr8(&f.Text, addr)
-	} else {
-		var checked link.L
+	asm.Trap(f, trap.MemoryOutOfBounds)
 
-		in.JLcb.Stub8(&f.Text)
-		checked.AddSite(f.Text.Addr)
-
-		f.MemoryOutOfBounds.Init(f)
-		asm.Trap(f, trap.MemoryOutOfBounds)
-
-		checked.Addr = f.Text.Addr
-		isa.UpdateNearBranches(f.Text.Bytes(), &checked)
-	}
+	checked.Addr = f.Text.Addr
+	isa.UpdateNearBranches(f.Text.Bytes(), &checked)
 
 	base = in.BaseScratch
 	disp = -int32(sizeReach)

internal/gen/func.go
@@ -69,8 +69,6 @@ type Func struct {
 
 	BranchTargets []*BranchTarget
 	BranchTables  []BranchTable
-
-	MemoryOutOfBounds TrapTrampoline
 }
 
 func (f *Func) LocalOffset(index int) int32 {

commit 2fc77edd7b87799d2ed5daf05a36b0d4cb2a811e

x86: avoid address calculation in simple branches
benchmark                 old ns/op     new ns/op     delta
BenchmarkLoad-8           303237354     289297472     -4.60%
BenchmarkLoadEvents-8     306311074     288495335     -5.82%

Commit a88c38c4cce53b784832276a5027ad6ef3c60c0a caused significant slowdown
(around +9%).

internal/isa/x86/binary.go
@@ -225,16 +225,10 @@ func binaryIntDivmul(f *gen.Func, index uint8, a, b operand.O) operand.O {
 }
 
 func opCheckDivideByZero(f *gen.Func, t wa.Type, r reg.R) {
-	var end link.L
-
 	in.TEST.RegReg(&f.Text, t, r, r)
-	in.JNEcb.Stub8(&f.Text)
-	end.AddSite(f.Text.Addr)
-
-	asm.Trap(f, trap.IntegerDivideByZero)
-
-	end.Addr = f.Text.Addr
-	isa.UpdateNearBranches(f.Text.Bytes(), &end)
+	in.JNEcb.Rel8(&f.Text, in.CALLcd.Size()) // Skip next instruction.
+	in.CALLcd.Addr32(&f.Text, f.TrapLinks[trap.IntegerDivideByZero].Addr)
+	f.MapCallAddr(f.Text.Addr)
 }
 
 func binaryIntShift(f *gen.Func, index uint8, a, b operand.O) operand.O {

internal/isa/x86/in/encode.go
@@ -544,6 +544,10 @@ type Dd byte    // opcode byte
 type D2d uint16 // two opcode bytes
 type D12 uint32 // combination
 
+func (Db) Size() int8  { return 2 }
+func (Dd) Size() int8  { return 5 }
+func (D2d) Size() int8 { return 6 }
+
 func (ops D12) Addr(text *code.Buf, addr int32) {
 	const (
 		insnSize8  = 2
@@ -565,6 +569,13 @@ func (ops D12) Addr(text *code.Buf, addr int32) {
 	disp32 := addrDisp(text.Addr, insnSize32, addr)
 	o.word(uint16(ops >> 16))
 	o.int32(disp32)
+	o.copy(text.Extend(o.len()))
+}
+
+func (op Db) Rel8(text *code.Buf, disp int8) {
+	var o output
+	o.byte(byte(op))
+	o.int8(disp)
 	o.copy(text.Extend(o.len()))
 }
 

internal/isa/x86/macro.go
@@ -427,20 +427,13 @@ func (MacroAssembler) Return(p *gen.Prog, numStackValues int) {
 }
 
 func (MacroAssembler) SetupStackFrame(f *gen.Func) (stackCheckAddr int32) {
-	var checked link.L
-
 	in.LEA.RegStackStub32(&f.Text, wa.I64, RegScratch)
 	stackCheckAddr = f.Text.Addr
 
 	in.CMP.RegReg(&f.Text, wa.I64, RegScratch, RegStackLimit)
-
-	in.JGEcb.Stub8(&f.Text)
-	checked.AddSite(f.Text.Addr)
-
-	asm.Trap(f, trap.CallStackExhausted) // handler checks if it was actually suspension
-
-	checked.Addr = f.Text.Addr
-	isa.UpdateNearBranches(f.Text.Bytes(), &checked)
+	in.JGEcb.Rel8(&f.Text, in.CALLcd.Size())                             // Skip next instruction.
+	in.CALLcd.Addr32(&f.Text, f.TrapLinks[trap.CallStackExhausted].Addr) // Handler checks suspension.
+	f.MapCallAddr(f.Text.Addr)
 	return
 }
 
@@ -521,27 +514,22 @@ func (MacroAssembler) Trap(f *gen.Func, id trap.ID) {
 }
 
 func (MacroAssembler) TrapIfLoopSuspended(f *gen.Func) {
-	var skip link.L
-
 	in.TEST8i.OneSizeRegImm(&f.Text, RegSuspendBit, 1)
-	in.JEcb.Stub8(&f.Text) // 0 -> skip, 1 -> trap
-	skip.AddSite(f.Text.Addr)
-
-	asm.Trap(f, trap.Suspended)
-
-	skip.Addr = f.Text.Addr
-	isa.UpdateNearBranches(f.Text.Bytes(), &skip)
+	in.JEcb.Rel8(&f.Text, in.CALLcd.Size()) // Skip next instruction if bit is zero (no trap).
+	in.CALLcd.Addr32(&f.Text, f.TrapLinks[trap.Suspended].Addr)
+	f.MapCallAddr(f.Text.Addr)
 }
 
 func (MacroAssembler) TrapIfLoopSuspendedSaveInt(f *gen.Func, saveReg reg.R) {
 	var skip link.L
 
 	in.TEST8i.OneSizeRegImm(&f.Text, RegSuspendBit, 1)
-	in.JEcb.Stub8(&f.Text) // 0 -> skip, 1 -> trap
+	in.JEcb.Stub8(&f.Text) // Skip if bit is zero (no trap).
 	skip.AddSite(f.Text.Addr)
 
 	in.PUSH.Reg(&f.Text, in.OneSize, saveReg)
-	asm.Trap(f, trap.Suspended)
+	in.CALLcd.Addr32(&f.Text, f.TrapLinks[trap.Suspended].Addr)
+	f.MapCallAddr(f.Text.Addr)
 	in.POP.Reg(&f.Text, in.OneSize, saveReg)
 
 	skip.Addr = f.Text.Addr
@@ -550,7 +538,7 @@ func (MacroAssembler) TrapIfLoopSuspendedSaveInt(f *gen.Func, saveReg reg.R) {
 
 func (MacroAssembler) TrapIfLoopSuspendedElse(f *gen.Func, elseAddr int32) {
 	in.TEST8i.OneSizeRegImm(&f.Text, RegSuspendBit, 1)
-	in.JEcd.Addr32(&f.Text, elseAddr) // 0 -> else, 1 -> trap
+	in.JEcd.Addr32(&f.Text, elseAddr) // Branch to else if bit is zero (no trap).
 
 	asm.Trap(f, trap.Suspended)
 }

internal/isa/x86/memory.go
@@ -143,16 +143,10 @@ func checkAccess(f *gen.Func, sizeReach uint64, index operand.O, offset uint32) 
 		}
 	}
 
-	var checked link.L
-
 	in.CMP.RegReg(&f.Text, wa.I64, RegScratch, RegMemoryLimit)
-	in.JLcb.Stub8(&f.Text)
-	checked.AddSite(f.Text.Addr)
-
-	asm.Trap(f, trap.MemoryOutOfBounds)
-
-	checked.Addr = f.Text.Addr
-	isa.UpdateNearBranches(f.Text.Bytes(), &checked)
+	in.JLcb.Rel8(&f.Text, in.CALLcd.Size()) // Skip next instruction.
+	in.CALLcd.Addr32(&f.Text, f.TrapLinks[trap.MemoryOutOfBounds].Addr)
+	f.MapCallAddr(f.Text.Addr)
 
 	base = in.BaseScratch
 	disp = -int32(sizeReach)

compile/benchmark_test.go
@@ -43,7 +43,7 @@ const (
 	loadBenchmarkEntrySymbol = "run"
 	loadBenchmarkMaxTextSize = 16 * 1024 * 1024
 	loadBenchmarkMaxDataSize = 16 * 1024 * 1024
-	loadBenchmarkTextCRC32   = 0x2f41f0da
+	loadBenchmarkTextCRC32   = 0xd9295c43
 	loadBenchmarkIgnoreCRC32 = false
 )
 