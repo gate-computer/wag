b016e2521ae9c8366fbe9073e85a9ce15fc2264b
Timo Savola <timo.savola@iki.fi>
2018-12-19 03:32:17 +0200

    compile: update benchmark checksum

--- compile/benchmark_test.go
+++ compile/benchmark_test.go
@@ -43,7 +43,7 @@ const (
 	loadBenchmarkEntrySymbol = "run"
 	loadBenchmarkMaxTextSize = 16 * 1024 * 1024
 	loadBenchmarkMaxDataSize = 16 * 1024 * 1024
-	loadBenchmarkTextCRC32   = 0x19fce57f
+	loadBenchmarkTextCRC32   = 0x20128dd0
 	loadBenchmarkIgnoreCRC32 = false
 )
 
