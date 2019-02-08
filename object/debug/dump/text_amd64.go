c70d25caa03a2a7d84be7beb1703b1707c76bb25
Timo Savola <timo.savola@iki.fi>
2019-02-08 22:35:03 +0200

    dump: fix x86 register names

--- object/debug/dump/text_amd64.go
+++ object/debug/dump/text_amd64.go
@@ -71,28 +71,35 @@ func rewriteText(insns []gapstone.Instruction, targets map[uint]string, textAddr
 		insn.OpStr = strings.Replace(insn.OpStr, "%r8b", "r8b", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r8w", "r8w", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r8d", "r8", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r8", "r8", -1)
 
 		insn.OpStr = strings.Replace(insn.OpStr, "%r9b", "r9b", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r9w", "r9w", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r9d", "r9", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r9", "r9", -1)
 
 		insn.OpStr = strings.Replace(insn.OpStr, "%r10b", "r10b", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r10w", "r10w", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r10d", "r10", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r10", "r10", -1)
 
 		insn.OpStr = strings.Replace(insn.OpStr, "%r11b", "r11b", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r11w", "r11w", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r11d", "r11", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r11", "r11", -1)
 
 		insn.OpStr = strings.Replace(insn.OpStr, "%r12b", "r12b", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r12w", "r12w", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r12d", "r12", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r12", "r12", -1)
 
-		insn.OpStr = strings.Replace(insn.OpStr, "%r13", "memorylimit", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r13b", "r13b", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r13w", "r13w", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r13d", "r13", -1)
+		insn.OpStr = strings.Replace(insn.OpStr, "%r13", "r13", -1)
+
 		insn.OpStr = strings.Replace(insn.OpStr, "%r14", "memory", -1)
 		insn.OpStr = strings.Replace(insn.OpStr, "%r15", "text", -1)
-
-		insn.OpStr = strings.Replace(insn.OpStr, "%mm1", "memorygrowlimit", -1)
 
 		if insn.Address < firstFuncAddr && (insn.Mnemonic == "movl" || insn.Mnemonic == "shlq") && strings.HasPrefix(insn.OpStr, "$") && strings.HasSuffix(insn.OpStr, ", result") {
 			var n uint
