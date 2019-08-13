df897c411d7650fca8de59642117c333c5217109
Timo Savola <timo.savola@iki.fi>
2019-08-14 00:08:52 +0300

    isa: arm: test: check for cgo

---
+++ internal/isa/arm/nop_test.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Timo Savola. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build arm64,!cgo
+
+package arm
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	t.Skip("tests require cgo")
+}
