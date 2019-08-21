23721faa867e07132f0301639fa548c40eb4fcc1
Timo Savola <timo.savola@iki.fi>
2019-08-21 23:53:52 +0300

    testdata: rust: rebuild with latest rustc

--- testdata/rust/test.rs
+++ testdata/rust/test.rs
@@ -1,23 +1,19 @@
-// rustc --target=wasm32-unknown-unknown -C opt-level=z -o test.wasm test.rs
+// rustc --target=wasm32-unknown-unknown --crate-type=cdylib -C opt-level=z -o test.wasm test.rs
 
 #![no_std]
-#![feature(panic_implementation)]
-#![feature(start)]
 
 extern "C" {
     fn write(fd: i32, data: *const u8, size: usize);
     fn _exit(status: i32) -> !;
 }
 
-#[start]
-pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+#[no_mangle]
+pub fn main() {
     let s = "hello, world\n";
     unsafe { write(0, s.as_ptr(), s.len()) }
-    0
 }
 
-#[no_mangle]
-#[panic_implementation]
+#[panic_handler]
 pub fn panic_fmt(_info: &::core::panic::PanicInfo) -> ! {
     let s = "panic\n";
     unsafe {
