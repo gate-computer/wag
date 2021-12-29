d44a9abf2d5548424aa71588d1c943d717f7ddd5
Timo Savola <timo.savola@iki.fi>
2021-12-29 06:26:25 +0000

    isa/arm: fix build constraints

--- internal/isa/arm/asm_test.s
+++ internal/isa/arm/asm_test.s
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build arm64 && cgo
-// +build arm64,cgo
+//go:build arm64 && !wagamd64 && cgo
+// +build arm64,!wagamd64,cgo
 
 #include "textflag.h"
 
--- internal/isa/arm/imm_test.go
+++ internal/isa/arm/imm_test.go
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (arm64 || wagarm64) && !wagamd64 && cgo
-// +build arm64 wagarm64
-// +build !wagamd64
-// +build cgo
+//go:build arm64 && !wagamd64 && cgo
+// +build arm64,!wagamd64,cgo
 
 package arm
 
